4ec48a9c059035b69e7b9fed8d5032d987ba1ee5
abdalla-amin <abdalla.amin@vodafone.com>
2023-01-05 05:29:12 +0200

    redeploy

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -6,7 +6,7 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 const TopArtists = () => {
     const { data, isFetching, error } = useGetTopChartsQuery();
 
-    if (isFetching) return <Loader title="Loading artists..." />;
+    if (isFetching) return <Loader title="Loading artists...." />;
 
     if (error) return <Error />;
 
